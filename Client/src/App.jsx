80f7dee0dbe9d889846405613ee6330ab4c3d1b7
nerdharsh <nerdharsh@gmail.com>
2024-04-15 05:15:49 +0530

    create pages and routes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
 
 export default function App() {
   return (
-    <h1 className='text-3xl text-red-500'>Tech Lab</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-Up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>  
+    
+  
   )
 }
---
+++ Client/src/pages/SignUp.jsx
@@ -0,0 +1,10 @@
+import App from "../App"
+
+export default function SignUp() {
+  return (
+    <div>
+        SignUp
+    </div>
+      
+  )
+}
